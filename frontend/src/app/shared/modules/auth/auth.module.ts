6db9880de78e40d434b6885336d20737cc364411
Limbo2332 <vorobiovalosha@gmail.com>
2023-10-13 19:09:57 +0300

    added reset password functionallity

--- frontend/src/app/shared/modules/auth/auth.module.ts
+++ frontend/src/app/shared/modules/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from '../../shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 @NgModule({
-  declarations: [SignInComponent, SignUpComponent],
+  declarations: [SignInComponent, SignUpComponent, ResetPasswordComponent],
   imports: [SharedModule, AuthRoutingModule],
 })
 export class AuthModule {}
