a788e65cc60cd58e2aa0fb655b9fa8a6b8e4cd8d
Limbo2332 <vorobiovalosha@gmail.com>
2023-10-16 22:02:14 +0300

    added entities

---
+++ backend/ChatApp.DAL/Entities/RefreshToken.cs
@@ -0,0 +1,22 @@
+﻿using ChatApp.DAL.Entities.Abstract;
+
+namespace ChatApp.DAL.Entities
+{
+    public class RefreshToken : BaseEntity
+    {
+        private const int DAYS_TO_EXPIRE = 5;
+
+        public RefreshToken()
+        {
+            Expires = DateTime.UtcNow.AddDays(DAYS_TO_EXPIRE);
+        }
+
+        public string Token { get; set; } = string.Empty;
+        public DateTime Expires { get; set; }
+
+        public bool IsActive => DateTime.UtcNow <= Expires;
+
+        public User? User { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ backend/ChatApp.DAL/Entities/UserChats.cs
@@ -0,0 +1,15 @@
+﻿using ChatApp.DAL.Entities.Abstract;
+
+namespace ChatApp.DAL.Entities
+{
+    public class UserChats : BaseEntity
+    {
+        public User? User { get; set; }
+        public int UserId { get; set; }
+
+        public Chat? Chat { get; set; }
+        public int ChatId { get; set; }
+
+        public bool IsSender { get; set; }
+    }
+}
