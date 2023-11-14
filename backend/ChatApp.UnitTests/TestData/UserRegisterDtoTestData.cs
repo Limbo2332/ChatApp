623ead441fdacbcbc708fa1b75fd54ff643a35a5
Limbo2332 <vorobiovalosha@gmail.com>
2023-11-14 20:40:06 +0200

    added unit tests for authcontroller

---
+++ backend/ChatApp.UnitTests/TestData/UserRegisterDtoTestData.cs
@@ -0,0 +1,39 @@
+﻿using ChatApp.Common.DTO.User;
+
+namespace ChatApp.UnitTests.TestData
+{
+    public class UserRegisterDtoTestData : TheoryData<UserRegisterDto>
+    {
+        public UserRegisterDtoTestData()
+        {
+            Add(new UserRegisterDto());
+            Add(new UserRegisterDto()
+            {
+                Email = "correctEmail@gmail.com"
+            });
+            Add(new UserRegisterDto()
+            {
+                Email = "correctEmail@gmail.com",
+                Password = "correctPassword1!"
+            });
+            Add(new UserRegisterDto()
+            {
+                Email = "correctEmail@gmail.com",
+                Password = "wrongPass",
+                UserName = "correctUserName"
+            });
+            Add(new UserRegisterDto()
+            {
+                Email = "wrongEmail",
+                Password = "correctPassword1!",
+                UserName = "correctUserName"
+            });
+            Add(new UserRegisterDto()
+            {
+                Email = "wrongEmail",
+                Password = "wrongPass",
+                UserName = "correctUserName"
+            });
+        }
+    }
+}
